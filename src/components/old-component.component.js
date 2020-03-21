eb1d09d7b9b10dfdc1afe3039abd556472c915e7
thakorraj <thakorraj1998@gmail.com>
2020-03-20 20:54:33 -0400

    Done

--- src/components/old-component.component.js
+++ src/components/old-component.component.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const OldComponent = props => {
     console.log("old ma aave che" + props.data);
     console.log(window.location.pathname.toString().substring(4));
-    let a;
+    let a='Bearer '+window.location.pathname.toString().substring(4)+"";
+    console.log(a);
     function sayHello() {
         
         const product = {
@@ -40,7 +41,7 @@ const OldComponent = props => {
         }
         const headers = {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJub25jZSI6Imw1TzZrQmdTTzd1VFRnM1dMZzQ3cHpSNDFubHFmVmt4aTRoV2pNU1dYZlUiLCJhbGciOiJSUzI1NiIsIng1dCI6IllNRUxIVDBndmIwbXhvU0RvWWZvbWpxZmpZVSIsImtpZCI6IllNRUxIVDBndmIwbXhvU0RvWWZvbWpxZmpZVSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC8zYjg1YTkwYS1kMGU0LTRmMmEtYmJjMS03OTExYzIzNmI2N2EvIiwiaWF0IjoxNTg0NzIzNjk1LCJuYmYiOjE1ODQ3MjM2OTUsImV4cCI6MTU4NDcyNzU5NSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFTUUEyLzhPQUFBQXEvcWJTbFBkOE1OdmtTQ2pTVDUyNDhTcE5FYVE2eXF6Y3MrTkdjclA3QzQ9IiwiYW1yIjpbInB3ZCJdLCJhcHBfZGlzcGxheW5hbWUiOiJUZXN0IEFwcCIsImFwcGlkIjoiY2Q5MDJjYWUtMzYzMy00ZWViLTllY2UtOTgwODYyMDBiZTk5IiwiYXBwaWRhY3IiOiIxIiwiZmFtaWx5X25hbWUiOiJQYXRlbCIsImdpdmVuX25hbWUiOiJIYXJzaGlsIiwiaXBhZGRyIjoiOTkuMjUyLjQ2LjE2MSIsIm5hbWUiOiJIYXJzaGlsIFBhdGVsIiwib2lkIjoiZTZmYThiMDQtZTA0Yy00ZjdiLWFjZGItOTk4Y2E3NTE3NGQ4IiwicGxhdGYiOiIzIiwicHVpZCI6IjEwMDMyMDAwOUZGNUVEMkEiLCJzY3AiOiJDYWxlbmRhcnMuUmVhZCBDYWxlbmRhcnMuUmVhZC5TaGFyZWQgQ2FsZW5kYXJzLlJlYWRXcml0ZSBDYWxlbmRhcnMuUmVhZFdyaXRlLlNoYXJlZCBDb250YWN0cy5SZWFkIGVtYWlsIE1haWwuUmVhZCBNYWlsLlJlYWRXcml0ZSBNYWlsLlNlbmQgTWFpbGJveFNldHRpbmdzLlJlYWQgTWFpbGJveFNldHRpbmdzLlJlYWRXcml0ZSBvcGVuaWQgcHJvZmlsZSBVc2VyLlJlYWQiLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiIxbDk5NGVtWkRhQVVReHNPNHJDU205dG1KXzlxVUtrSXBoUUVvV19EcGp3IiwidGlkIjoiM2I4NWE5MGEtZDBlNC00ZjJhLWJiYzEtNzkxMWMyMzZiNjdhIiwidW5pcXVlX25hbWUiOiJwYXRlbGhhcnNoaWxAcGF0ZWxoYXJzaGlsLm9ubWljcm9zb2Z0LmNvbSIsInVwbiI6InBhdGVsaGFyc2hpbEBwYXRlbGhhcnNoaWwub25taWNyb3NvZnQuY29tIiwidXRpIjoicERISk1SamFma2lLSGUtck54OWJBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiNjJlOTAzOTQtNjlmNS00MjM3LTkxOTAtMDEyMTc3MTQ1ZTEwIl0sInhtc19zdCI6eyJzdWIiOiJseWpOZ1VHYnV4bnVJSk41MEhma3NCTXd2djRFbW52alRnZFlLRFJMSGZJIn0sInhtc190Y2R0IjoxNTgzMzc1OTQ5fQ.mTKK4q3SYKyZJOv8OrPreOVkcGeRQT3aw4IhC2X-2DdBb-qqCOzYUnqGpQedr6RhGSOodbdeyuAzAThXgEpH2dNuHNDxJOGB95cayUN_-Aw9Eb4cYfzt0o_aM3_wPHFp6DzBlK9JjF_cKEbByKlIDD3YXpGXBXuj-Q1AMlN7IPQvwG63yYf6Xjq9U3N5YAFVrdiOqZUyec_5ou8XQXe6z3MLEwtWayYHO_8RsqY7Y_4uzkwjU1a7D-sd6N0FJLwXI8c9os4RozSYhqEDYVtCo6CIIAu1ZizlGynvXgXyr-biGZbzjRQHqCd7zi3nPUlUV0i1IDHPMKfwafJgX5S0_w'
+            'Authorization': a
           }
 
         // console.log(store);
